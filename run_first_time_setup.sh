252b3983b39191cd114fe44b3dd85c0a18e51fa2
TheBWitty <luigichilling@gmail.com>
2022-03-15 13:33:38 -0500

    fix script to work properly

--- run_first_time_setup.sh
+++ run_first_time_setup.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-PACKER_SSH_PRIV="common/ssh_keys_for_packer/"
+PACKER_SSH_PRIV="common/ssh_keys_for_packer"
 
 border () {
 	echo "================================================================================"
@@ -16,8 +16,12 @@ border
 # Generate (if needed) and maintain proper file permissions for keys
 if [[ ! -e "$PACKER_SSH_PRIV"/id_rsa ]] ; then
 	echo "Keys not found... generating now!"
-	ssh-keygen -f "$PACKER_SSH_PRIV"/id_rsa -P "" && cp -v id_rsa.pub authorized_keys
+	border
+	pushd "$PACKER_SSH_PRIV"
+	ssh-keygen -f id_rsa -P "" && cp -v id_rsa.pub authorized_keys
+	popd
+else
+	echo "Keys already exist in $PACKER_SSH_PRIV... *not* re-generating!"
 fi
-chmod -v 600 "$PACKER_SSH_PRIV"
-# chown -Rv "$NEW_UID":"$NEW_GUID" .
+chmod -v 600 "$PACKER_SSH_PRIV"/id_rsa
 border
