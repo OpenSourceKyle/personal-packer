8b1f661943f0ac6a3f6410f5442c8ad148797280
TheBWitty <luigichilling@gmail.com>
2022-07-27 19:46:08 -0500

    WIP: fix found... just tweaking GRUB/EFI options

--- common/http/arch/install-base.sh
+++ common/http/arch/install-base.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Adapted from: https://github.com/conao3/packer-manjaro/blob/master/scripts/install-base.sh
 # Reference: https://www.tecmint.com/arch-linux-installation-and-configuration-guide/
+# Reference: https://github.com/badele/archlinux-auto-install/blob/main/install/install.sh
 
 set -eux
 
@@ -39,10 +40,11 @@ fi
 /usr/bin/dd if=/dev/zero of=${DISK} bs=512 count=2048
 /usr/bin/wipefs --all ${DISK}
 
-# Create EFI partition: 300MB size, type EFI (ef00), named, attribute bootable
-/usr/bin/sgdisk --new=1:0:+300M --typecode=1:ef00 --change-name=1:efi --attributes=1:set:2 ${DISK}
+# Create EFI partition: size, type EFI (ef00), named, attribute bootable
+/usr/bin/sgdisk --new=1:0:+550M --typecode=1:ef00 --change-name=1:efi --attributes=1:set:2 ${DISK}
 # Create root partition: remaining free space, type Linux (8300), named
 /usr/bin/sgdisk --new=2:0:0 --typecode=2:8300 --change-name=2:root ${DISK}
+/usr/bin/sgdisk -p ${DISK}
 
 # Creating /boot filesystem (FAT32)
 /usr/bin/mkfs.fat -F32 ${DISK_PART_BOOT}
@@ -61,6 +63,7 @@ fi
 # Need to install netctl as well: https://github.com/archlinux/arch-boxes/issues/70
 # Can be removed when user's Arch plugin will use systemd-networkd: https://github.com/hashicorp/vagrant/pull/11400
 /usr/bin/yes | /usr/bin/pacstrap ${CHROOT_MOUNT} base base-devel linux archlinux-keyring gptfdisk openssh syslinux dhcpcd netctl python vim grub efibootmgr dosfstools os-prober mtools rng-tools
+/usr/bin/genfstab -U -p ${CHROOT_MOUNT} >> ${CHROOT_MOUNT}/etc/fstab
 
 # System configuration
 /usr/bin/arch-chroot ${CHROOT_MOUNT} echo '${HOSTNAME}' > /etc/hostname
@@ -76,11 +79,14 @@ fi
 # Workaround for https://bugs.archlinux.org/task/58355 which prevents sshd to accept connections after reboot
 /usr/bin/arch-chroot ${CHROOT_MOUNT} /usr/bin/systemctl enable rngd
 
-/usr/bin/mount -o noatime,errors=remount-ro --mkdir ${DISK_PART_BOOT} ${CHROOT_MOUNT}/boot/EFI
-# Generate the filesystem table
-/usr/bin/genfstab -U -p ${CHROOT_MOUNT} > ${CHROOT_MOUNT}/etc/fstab
+/usr/bin/mount -o noatime,errors=remount-ro --mkdir ${DISK_PART_BOOT} ${CHROOT_MOUNT}/boot/efi
+/usr/bin/arch-chroot ${CHROOT_MOUNT} ls -laR /boot/
 # Install GRUB UEFI
-/usr/bin/arch-chroot ${CHROOT_MOUNT} grub-install --target=x86_64-efi --bootloader-id=grub_uefi --efi-directory=${CHROOT_MOUNT}/boot/EFI --recheck $DISK
-/usr/bin/arch-chroot ${CHROOT_MOUNT} grub-mkconfig -o /boot/grub/grub.cfg
+/usr/bin/arch-chroot ${CHROOT_MOUNT} /usr/bin/grub-install --target=x86_64-efi --bootloader-id=boot --efi-directory=/boot/efi $DISK
+/usr/bin/arch-chroot ${CHROOT_MOUNT} /usr/bin/grub-mkconfig -o /boot/grub/grub.cfg
+# https://www.reddit.com/r/archlinux/comments/5mqheo/comment/dc6ufmq/?utm_source=reddit&utm_medium=web2x&context=3
+# https://askubuntu.com/questions/566315/virtualbox-boots-only-in-uefi-interactive-shell
+/usr/bin/arch-chroot ${CHROOT_MOUNT} /usr/bin/cp -rv /boot/efi/EFI/boot/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
+/usr/bin/arch-chroot ${CHROOT_MOUNT} ls -laR /boot/
 
 echo "===>>> INSTALLATION COMPLETE <<<==="
